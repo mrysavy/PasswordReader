c243bfaac093f82b705355011aec699bf34444e0
Michal Rysavy <mrysavy@trask.cz>
2015-05-26 21:49:26 +0200

    Increased version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PasswordReader',
-      version='0.1',
+      version='0.1.1',
       description='PasswordReader tool',
       author='Michal Rysavy',
       packages=['passwordreader'],
